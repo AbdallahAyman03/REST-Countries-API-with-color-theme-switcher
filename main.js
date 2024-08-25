a006a853e18e8cb11c9d29c16df6b36c017ea0e8
Abdallah Ayman <149713232+AbdallahAyman03@users.noreply.github.com>
2024-08-26 01:10:16 +0300

    main JavaScipt

--- main.js
+++ main.js
@@ -6,23 +6,96 @@ let search = document.querySelector("#search");
 let searchBtn = document.querySelector("#search-button");
 let searchBar = document.querySelector(".search-bar");
 let filter = document.querySelector("#region");
+let countries = document.querySelector("#countries");
 
-darkBtn.addEventListener("click", function () {
-  //   this.textContent = "Light Mode";
-  this.style.backgroundColor = "hsl(210deg 21.82% 21.57%)";
-  this.style.color = "#ffffff";
-  //   this.setAttribute("id", "light-switcher");
-  body.style.backgroundColor = "hsl(204.55deg 25.58% 16.86%)";
-  header.style.backgroundColor = "hsl(210deg 21.82% 21.57%)";
-  h1.style.color = "#ffffff";
-  search.style.backgroundColor = "hsl(210deg 21.82% 21.57%)";
-  search.style.backgroundColor = "hsl(210deg 21.82% 21.57%)";
-  searchBtn.style.color = "hsl(0deg 0% 70%)";
-  searchBtn.style.backgroundColor = "hsl(210deg 21.82% 21.57%)";
-  searchBar.style.backgroundColor = "hsl(210deg 21.82% 21.57%)";
-  filter.style.backgroundColor = "hsl(210deg 21.82% 21.57%)";
-  filter.style.color = "hsl(0deg 0% 70%)";
+let darkMode = false;
+let countryElm;
+const loadData = async (region) => {
+  fetch("data.json")
+    .then((response) => response.json())
+    .then((data) => {
+      data.forEach((country) => {
+        let countryCard = document.createElement("div");
+        countryCard.classList.add("country");
+        countryCard.innerHTML = `
+      <div class="flag">
+        <img src="${country.flag}" alt="flag" />
+      </div>
+      <div class="country-info">
+        <h2>${country.name}</h2>
+        <p><strong>Population:</strong> ${country.population}</p>
+        <p><strong>Region:</strong> ${country.region}</p>
+        <p><strong>Capital:</strong> ${country.capital}</p>
+      </div>
+      `;
+        if (region === "All" || region === country.region)
+          countries.insertAdjacentHTML("beforeend", countryCard.outerHTML);
+      });
+      countryElm = document.querySelectorAll(".country");
+      if (darkMode) {
+        countryElm.forEach((country) => {
+          country.style.backgroundColor = "hsl(210deg 21.82% 21.57%)";
+          country.style.color = "#ffffff";
+        });
+      } else {
+        countryElm.forEach((country) => {
+          country.style.backgroundColor = "#ffffff";
+          country.style.color = "hsl(0, 0%, 17%)";
+        });
+      }
+    });
+};
+
+loadData("All");
+filter.addEventListener("change", function () {
+  countries.innerHTML = "";
+  loadData(this.value);
 });
+
+let changeTheme = function () {
+  countryElm = document.querySelectorAll(".country");
+  if (darkMode) {
+    darkMode = false;
+    this.style.backgroundColor = "#ffffff";
+    this.style.color = "hsl(210deg 21.82% 21.57%)";
+    body.style.backgroundColor = "#ffffff";
+    header.style.backgroundColor = "#ffffff";
+    h1.style.color = "hsl(0, 0%, 17%)";
+    search.style.backgroundColor = "#ffffff";
+    search.style.backgroundColor = "#ffffff";
+    searchBtn.style.color = "hsl(0, 0%, 17%)";
+    searchBtn.style.backgroundColor = "#ffffff";
+    searchBar.style.backgroundColor = "#ffffff";
+    filter.style.backgroundColor = "#ffffff";
+    filter.style.color = "hsl(0, 0%, 17%)";
+    countryElm.forEach((country) => {
+      country.style.backgroundColor = "#ffffff";
+      country.style.color = "hsl(0, 0%, 17%)";
+    });
+  } else {
+    darkMode = true;
+    //   this.textContent = "Light Mode";
+    this.style.backgroundColor = "hsl(210deg 21.82% 21.57%)";
+    this.style.color = "#ffffff";
+    //   this.setAttribute("id", "light-switcher");
+    body.style.backgroundColor = "hsl(204.55deg 25.58% 16.86%)";
+    header.style.backgroundColor = "hsl(210deg 21.82% 21.57%)";
+    h1.style.color = "#ffffff";
+    search.style.backgroundColor = "hsl(210deg 21.82% 21.57%)";
+    search.style.backgroundColor = "hsl(210deg 21.82% 21.57%)";
+    searchBtn.style.color = "hsl(0deg 0% 70%)";
+    searchBtn.style.backgroundColor = "hsl(210deg 21.82% 21.57%)";
+    searchBar.style.backgroundColor = "hsl(210deg 21.82% 21.57%)";
+    filter.style.backgroundColor = "hsl(210deg 21.82% 21.57%)";
+    filter.style.color = "hsl(0deg 0% 70%)";
+
+    countryElm.forEach((country) => {
+      country.style.backgroundColor = "hsl(210deg 21.82% 21.57%)";
+      country.style.color = "#ffffff";
+    });
+  }
+};
+darkBtn.addEventListener("click", changeTheme);
 
 // document.addEventListener("click", function (element) {
 //   if (element.target.getAttribute("id") === "light-switcher") {
